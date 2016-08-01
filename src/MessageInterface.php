45387fdc25bf8d02846d817c3d815dee6df0d78e
Filipe Silva <silvam.filipe@gmail.com>
2016-08-01 00:46:57 +0000

    Started message interface

---
+++ src/MessageInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of slick/mail package
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Slick\Mail;
+
+/**
+ * E-Mail Message Interface
+ *
+ * @package Slick\Mail
+ * @author  Filipe Silva <silvam.filipe@gmail.com>
+ */
+interface MessageInterface
+{
+
+}
